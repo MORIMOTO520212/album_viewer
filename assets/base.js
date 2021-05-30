7f00578bbd078ee4e94fe47722532318ce7b4050
MORIMOTO520212 <kanten520212@gmail.com>
2021-05-30 13:14:35 +0900

    2021.05.30

--- assets/base.js
+++ assets/base.js
@@ -6,11 +6,18 @@
     https://kats-eye.net/info/2019/02/22/drag_drop_01/
 */
 
-function transform(){ // カードの傾き
+function transForm(){ // カードの傾きの角度を返す
+    // Type：String
     let degree = [-25, -10, 0, 10, 25];
     return degree[Math.floor(Math.random()*degree.length)].toString();
 }
 
+function pxRange(min, max){ // 最小～最大の範囲でランダムな値を返す
+    // Type：String
+    return Math.floor(Math.random()*(max-min)+15).toString();
+}
+
+/* GET Tweet data */
 var tweet;
 $.post("assets/getData.php?mode=tweet", {}, function(data){
     console.log("get tweet data");
@@ -24,31 +31,21 @@ function main() {
     var mvs_y = 0;
     var tgt_id_no;
     var z_idx_ini = 100;
-    var X_STT_P = 0;
-    var Y_STT_P = 100;
     var img_wdt = 194;
     var img_hgt = 172;
-    var sW = window.innerWidth;
-    var sH = window.innerHeight;
-    var elm_div = document.getElementById("album");
-    var rect = elm_div.getBoundingClientRect();
-    X_STT_P = Math.ceil(rect.left / 10) + 20; // x-axis init
-    Y_STT_P = Math.ceil(rect.top / 10) + 20;  // y-axis init
-
     var card_elm = document.getElementsByClassName("card");
     for(var i=0; i< card_elm.length; i++){
-        card_elm[i].style.cssText = "transform:rotate("+transform()+"deg)";
-        card_elm[i].style.top    = Y_STT_P+"px";
-        card_elm[i].style.left   = X_STT_P+i * img_wdt+"px";
+        card_elm[i].style.cssText = "transform:rotate("+transForm()+"deg);"; // degree
+        card_elm[i].style.top    = pxRange(40, 240)+"px"; // set card x-axis
+        card_elm[i].style.left   = pxRange(32, 695)+"px"; // set card y-axis
         card_elm[i].style.width  = img_wdt+"px";
         card_elm[i].style.height = img_hgt+"px";
         card_elm[i].style.zIndex = z_idx_ini;
         card_elm[i].style.backgroundColor = "#"+tweet[i][5]; // Card Color
-        //card_elm[i].innerText = tweet[i][7]; // tweet contents
         card_elm[i].children[1].innerText = tweet[i][7]; // Tweet Contents
     }
 
-    // MouseDown
+    /* MouseDown */
     window.onmousedown = function(event) {
         mv_fg = "true";
         tgt_id_no = event.target.id;
@@ -59,17 +56,23 @@ function main() {
         mvs_y = event.clientY - parseInt(document.getElementById(tgt_id_no+"_1").style.top.replace("px",""));  // set cursor position
     }
 
-    // MouseMove
+    /* MouseMove */
     window.onmousemove = function(event) {
+        /*
+            移動制限
+            top(y): 40-240px; left(x): 32-695px;
+        */
         if(mv_fg == "true") {
             var cur_x = event.clientX - mvs_x; // set cursor position
             var cur_y = event.clientY - mvs_y; // set cursor position
-            document.getElementById(tgt_id_no+"_1").style.left = cur_x + "px";
-            document.getElementById(tgt_id_no+"_1").style.top = cur_y + "px";
+            if((40 < cur_y && cur_y < 240) && (32 < cur_x && cur_x < 695)){
+                document.getElementById(tgt_id_no+"_1").style.left = cur_x + "px";
+                document.getElementById(tgt_id_no+"_1").style.top = cur_y + "px";
+            }
         }
     }
 
-    // MouseUp
+    /* MouseUp */
     window.onmouseup = function() { 
         mv_fg = "false";
         tgt_id_no = "";
